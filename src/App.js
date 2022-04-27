13f3822df531801c07102530db3794a1805d67a7
Reid Schroder <reidschroder2@gmail.com>
2022-04-27 17:17:47 -0400

    contact section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 import './App.css';
 
 function App() {
@@ -16,17 +17,27 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
   return (
     <div>
       <Nav
       categories={categories}
       setCurrentCategory={setCurrentCategory}
       currentCategory={currentCategory}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <Gallery
+        {!contactSelected ? (
+          <>
+          <Gallery
         currentCategory={currentCategory}></Gallery>
         <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+        
       </main>
     </div>
   );

4cfc7b389c22a88a5b68193e2d4095910fe4b281
Reid Schroder <reidschroder2@gmail.com>
2022-04-29 18:13:28 -0400

    added photo modals

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
-import './App.css';
 
 function App() {
   const [categories] = useState([
@@ -11,33 +10,33 @@ function App() {
       name: 'commercial',
       description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
-    { name: 'portraits', description: 'Portraits of people in my life'},
-    { name: 'food', description: 'Delicious delicacies'},
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature'},
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
       <Nav
-      categories={categories}
-      setCurrentCategory={setCurrentCategory}
-      currentCategory={currentCategory}
-      contactSelected={contactSelected}
-      setContactSelected={setContactSelected}
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
         {!contactSelected ? (
           <>
-          <Gallery
-        currentCategory={currentCategory}></Gallery>
-        <About></About>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
           </>
         ) : (
           <ContactForm></ContactForm>
         )}
-        
       </main>
     </div>
   );
